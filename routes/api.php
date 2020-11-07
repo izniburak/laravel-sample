f179c1e83ec5e711cb6641555680859e36b877b1
İzni Burak Demirtaş <izniburak@outlook.com>
2020-11-07 07:31:16 +0300

    Added routes.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('leagues', \App\Http\Controllers\LeaguesController::class);
+Route::post('leagues/{league}/fixture/generate', [\App\Http\Controllers\LeaguesController::class, 'generateFixture']);
+Route::post('leagues/{league}/fixture/simulate', [\App\Http\Controllers\LeaguesController::class, 'simulate']);
+Route::post('leagues/{league}/fixture/weekly-simulate', [\App\Http\Controllers\LeaguesController::class, 'simulateWeekly']);
+Route::get('leagues/{league}/fixture', [\App\Http\Controllers\LeaguesController::class, 'fixture']);
+Route::get('leagues/{league}/table', [\App\Http\Controllers\LeaguesController::class, 'table']);
+Route::apiResource('leagues.teams', \App\Http\Controllers\TeamsController::class);
+Route::post('leagues/{league}/teams/bulk', [\App\Http\Controllers\TeamsController::class, 'bulk']);
+Route::apiResource('leagues.matches', \App\Http\Controllers\MatchesController::class);
+
